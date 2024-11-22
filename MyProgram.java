0cf8b60d63d8b0dd2d7175adf4306a0a6654952d
AnanthV123 <ananth.ven312@gmail.com>
2024-11-22 11:34:19 -0500

    playing around with action listener for button created Scorecard class to keep track of a team's score in one match

--- MyProgram.java
+++ MyProgram.java
@@ -1,7 +1,31 @@
-public class MyProgram 
+import javax.swing.*;
+//import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyProgram
 {
     public static void main(String[] args)
     {
         System.out.println("hello world");
+        
+        JFrame f = new JFrame("Button Example");
+        JButton button = new JButton("clicker");
+        button.setBounds(50,100,95,30);
+        f.add(button);
+        f.setSize(400,400);
+        f.setLayout(null);
+        f.setVisible(true);
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        
+        // button.addActionListener(new ActionListener() {
+        //     Scorecard s = new Scorecard();
+        //     public void actionPerformed(ActionEvent e){  
+        //         button.setText("Yay");
+        //         s.setSpeakerScoreNum(s.getSpeakerScoreNum()+1);
+        //         System.out.println(s.getSpeakerScoreNum());
+        //     }   
+        // });
+        
     }
 }
---
+++ Scorecard.java
@@ -0,0 +1,66 @@
+public class Scorecard {
+    private int speakerScoreNum;
+    private int ampScoreNum;
+    private int autonScoreNum;
+    private boolean parked;
+    private boolean trap;
+    private boolean climb;
+    private int totalScore;
+
+    // Constructor
+    public Scorecard(){
+        speakerScoreNum = 0;
+        ampScoreNum = 0;
+        autonScoreNum = 0;
+        parked = false;
+        trap = false;
+        climb = false;
+        totalScore = 0;
+    }
+
+    // Getters
+    public int getSpeakerScoreNum(){
+        return speakerScoreNum;
+    }
+    public int getAmpScoreNum(){
+        return ampScoreNum;
+    }
+    public int getAutonScoreNum(){
+        return autonScoreNum;
+    }
+    public boolean getParked(){
+        return parked;
+    }
+    public boolean getTrap(){
+        return trap;
+    }
+    public boolean getClimb(){
+        return climb;
+    }
+    public int getTotalScore(){
+        return totalScore;
+    }
+
+    // Setters
+    public void setSpeakerScoreNum(int s){
+        speakerScoreNum = s;
+    }
+    public void setAmpScoreNum(int am){
+        ampScoreNum = am;
+    }
+    public void setAutonScoreNum(int au){
+        autonScoreNum = au;
+    }
+    public void setParked(boolean p){
+        parked = p;
+    }
+    public void setTrap(boolean t){
+        trap = t;
+    }
+    public void setClimb(boolean c){
+        climb = c;
+    }
+    public void setTotalScore(int ts){
+        totalScore = ts;
+    }
+}
